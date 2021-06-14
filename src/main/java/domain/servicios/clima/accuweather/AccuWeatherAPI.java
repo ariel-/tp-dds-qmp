8a479f5f46207cab1806a8d93f6eff803383fa51
ariel- <ariel-@users.noreply.github.com>
2021-06-14 17:30:23 -0300

    Feedback: mock class -> interface

--- src/main/java/domain/servicios/clima/accuweather/AccuWeatherAPI.java
+++ src/main/java/domain/servicios/clima/accuweather/AccuWeatherAPI.java
@@ -1,28 +1,10 @@
 package domain.servicios.clima.accuweather;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public abstract class AccuWeatherAPI {
-  public List<Map<String, Object>> getWeather(String ciudad) {
-    return Arrays.asList(new HashMap<String, Object>() {{
-      put("DateTime", "2019-05-03T01:00:00-03:00");
-      put("EpochDateTime", 1556856000);
-      put("WeatherIcon", 33);
-      put("IconPhrase", "Clear");
-      put("IsDaylight", false);
-      put("PrecipitationProbability", 0);
-      put("MobileLink", "http://m.accuweather.com/en/ar/villa-vil/7984/");
-      put("Link", "http://www.accuweather.com/en/ar/villa-vil/7984");
-      put("Temperature", new HashMap<String, Object>() {{
-        put("Value", 57);
-        put("Unit", "F");
-        put("UnitType", 18);
-      }});
-    }});
-  }
+public interface AccuWeatherAPI {
+  List<Map<String, Object>> getWeather(String ciudad);
 
-  public abstract Map<String, List<String>> getAlerts(String ciudad);
+  Map<String, List<String>> getAlerts(String ciudad);
 }
